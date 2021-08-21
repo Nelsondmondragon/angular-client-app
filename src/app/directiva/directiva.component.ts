4c9b8fe67d535557f4717f9a34e3f9af4dca8290
Nelson Cortes <nelsondmondragon@gmail.com>
2021-08-21 08:41:11 -0500

    Structural directive

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,13 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent {
+
+listCourse: string[] = ['TypeScript','JavaScript','JavaSE','C#','PHP'];
+  constructor() { }
+
+
+}
